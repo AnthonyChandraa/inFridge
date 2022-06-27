a34d0010baa22d7ec3f2beaa2d4f9e9f9f00931e
AnthonyChandraa <anthonychd.22@gmail.com>
2022-06-27 10:39:39 +0700

    Test

--- Master/resources/views/landing/start.blade.php
+++ Master/resources/views/landing/start.blade.php
@@ -3,9 +3,24 @@
 @section('title', 'inFridge | Start')
 
 @section('content')
-<div class="container-fluid">
-    <div class="container logo-container">
-
+<div class="container-fluid m-0 p-0" style="background-image: url('http://127.0.0.1:8000/storage/Assets/Home.jpg');
+         background-repeat: no-repeat; background-size: contain; background-position: right;">
+    <div class="container logo-container position-relative p-0 m-0 d-flex justify-content-end align-items-center"
+         style="width: 45vw; height:100vh; background-color: #0F225E">
+        <div class="position-absolute" style="left:38vw">
+            <div class="logo d-flex justify-content-center align-items-center">
+                <p class="m-2" style="font-size: 100px; font-family: 'Poppins', sans-serif; color: white; font-weight:
+                bold;
+">in</p>
+                <p class="m-2" style="font-size: 100px; font-family: 'Poppins', sans-serif; font-weight: bold;
+color: #0F225E">Fridge</p>
+            </div>
+            <div class="d-flex justify-content-center">
+                <a href="" id="btn-start" style="font-family: 'Poppins', sans-serif"><button
+                        style="width: 100px; border-radius: 45px; padding: 10px; border: 2px solid
+                        #0F225E; background-color: transparent;">Start</button></a>
+            </div>
+        </div>
     </div>
 </div>
 
